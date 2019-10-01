LineageOS/android_build

build: Conditionally include AudioPackage14.mk instead of AllAudio.mk
* AllAudio.mk doesn't contain latest material files and also
  contains multiple duplicate files. Just copy the latest
  material ones and we'll include the older files in vendor/lineage

Change-Id: Ic7353216a3f3dc3c51006d951ac932cfd6e5f3b5

target/product/gsi_common.mk
@@ -21,7 +21,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_product.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_product.mk)
 
 # Default AOSP sounds
+ifeq ($(LINEAGE_BUILD),)
 $(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
+else
+$(call inherit-product-if-exists, frameworks/base/data/sounds/AudioPackage14.mk)
+endif
 
 # Additional settings used in all AOSP builds
 PRODUCT_PRODUCT_PROPERTIES += \

NoSpamDan/build

target/product/gsi_common.mk
@@ -21,7 +21,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_product.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_product.mk)
 
 # Default AOSP sounds
+ifeq ($(CANDY_BUILD),)
 $(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
+else
+$(call inherit-product-if-exists, frameworks/base/data/sounds/AudioPackage14.mk)
+endif
 
 # Additional settings used in all AOSP builds
 PRODUCT_PRODUCT_PROPERTIES += \